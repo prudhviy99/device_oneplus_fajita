43082e41d4d8f218da662f2b433cadbd1738dbf8
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-05-29 18:49:58 +0530

    sdm845-common: rootdir: Add init script to create and modify swap
    Co-authored-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: I99f216a23e49adb4ee8b32f375a6f41b72ecba81

--- common.mk
+++ common.mk
@@ -67,7 +67,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.recovery.qcom.rc \
-    init.target.rc
+    init.target.rc \
+    init.swap.sh
 
 # Display
 PRODUCT_PACKAGES += \
